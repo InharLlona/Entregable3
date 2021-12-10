3cfb5cac14d2e450b14621c893db3c3ccb647990
InharLlona <inharllona@gmail.com>
2021-12-10 10:13:30 +0100

    basic

--- js.js
+++ js.js
@@ -6,26 +6,26 @@ const oEmpleado ={
     pagas:14,
 }
 
-
-
-if(oEmpleado.bruto < 12000){
-    Reduccion= 0;
-} else if(oEmpleado.bruto < 24000){
-    Reduccion = 0.08;
-}else if(oEmpleado.bruto < 34000){
-    Reduccion = 0.16;
-}else{
+if(oEmpleado.bruto < 12450){
+    Reduccion= 0.19;
+} else if(oEmpleado.bruto < 22200){
+    Reduccion = 0.24;
+}else if(oEmpleado.bruto < 35200){
     Reduccion = 0.30;
+}else if(oEmpleado.bruto < 60000){
+    Reduccion = 0.37;
+}else if(oEmpleado.bruto < 300000){
+    Reduccion = 0.45;
+}else {
+    Reduccion = 0.47;
 }
 
 if(oEmpleado.hijos>0){
     Reduccion = Reduccion - (0.02*oEmpleado.hijos);
 }
 
-
 NetoT = oEmpleado.bruto - oEmpleado.bruto* Reduccion;
 NetoM = NetoT / oEmpleado.pagas;
-
 
 console.log("Sueldo neto anual", NetoT.toFixed(2), "€");
 console.log("Sueldo neto mensual", NetoM.toFixed(2), "€");
